608d937385737e3e2c0bfbdfd13364145347ce82
Jesus Arredondo <chuyarredondo@gmail.com>
2017-12-15 04:45:45 -0600

    More changes

---
+++ SacramentMeetingApp/Models/SacramentViewModels/MeetingIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingApp.Models.SacramentViewModels
+{
+    public class MeetingIndexData
+    {
+        public IEnumerable<Song> Songs { get; set; }
+        public IEnumerable<Member> Members { get; set; }
+    }
+}
